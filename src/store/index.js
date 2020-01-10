ee28c2f893c1646168d83589c2975d7692eb0910
DavidTan0527 <david_weiucheng@hotmail.com>
2020-01-10 09:17:05 +0800

    Add offline vuex store storage

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,32 @@ import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    score: 0,
-  },
-  mutations: {
-    ADD_SCORE: (state, val) => state.score += val,
-    RESET_SCORE: (state) => state.score = 0,
-  },
-  plugins: [createPersistedState()],
-})
+const { offline, mixinOff } = storage.register('testapp', '1.0.0', 'store');
+
+// export default new Vuex.Store({
+//   state: {
+//     score: 0,
+//   },
+//   mutations: {
+//     ADD_SCORE: (state, val) => state.score += val,
+//     RESET_SCORE: (state) => state.score = 0,
+//   },
+//   plugins: [createPersistedState()],
+// })
+
+export default {
+  store: new Vuex.Store({
+    plugins: [createPersistedState(), offline.plugin],
+
+    actions: Object.assign({
+      score: 0,
+    }, offline.actions),
+
+    mutations: Object.assign({
+      ADD_SCORE: (state, val) => state.score += val,
+      RESET_SCORE: (state) => state.score = 0,
+    }, offline.mutations),
+  }),
+  // export mixi too
+  mixinOff
+}
