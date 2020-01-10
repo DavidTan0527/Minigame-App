b3b4be7cf109c76d01e91939a88c96510114bcc7
DavidTan0527 <david_weiucheng@hotmail.com>
2020-01-10 09:26:05 +0800

    Revert back to original vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -4,32 +4,33 @@ import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
-const { offline, mixinOff } = storage.register('testapp', '1.0.0', 'store');
+// import storage from 'vue-offline-worker';
+// const { offline, mixinOff } = storage.register('testapp', '1.0.0', 'store');
 
-// export default new Vuex.Store({
-//   state: {
-//     score: 0,
-//   },
-//   mutations: {
-//     ADD_SCORE: (state, val) => state.score += val,
-//     RESET_SCORE: (state) => state.score = 0,
-//   },
-//   plugins: [createPersistedState()],
-// })
+export default new Vuex.Store({
+  state: {
+    score: 0,
+  },
+  mutations: {
+    ADD_SCORE: (state, val) => state.score += val,
+    RESET_SCORE: (state) => state.score = 0,
+  },
+  plugins: [createPersistedState()],
+})
 
-export default {
-  store: new Vuex.Store({
-    plugins: [createPersistedState(), offline.plugin],
+// export default {
+//   store: new Vuex.Store({
+//     state: {
+//       score: 0,
+//     },
+    
+//     mutations: Object.assign({
+//       ADD_SCORE: (state, val) => state.score += val,
+//       RESET_SCORE: (state) => state.score = 0,
+//     }, offline.mutations),
 
-    actions: Object.assign({
-      score: 0,
-    }, offline.actions),
-
-    mutations: Object.assign({
-      ADD_SCORE: (state, val) => state.score += val,
-      RESET_SCORE: (state) => state.score = 0,
-    }, offline.mutations),
-  }),
-  // export mixi too
-  mixinOff
-}
+//     plugins: [createPersistedState(), offline.plugin],
+//   }),
+//   // export mixi too
+//   mixinOff
+// }
